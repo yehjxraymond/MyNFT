8f38376f7b0e0592e1a626ba2e690078a55484ca
Raymond Yeh <ray@geek.sg>
2021-06-05 14:56:19 +0800

    feat: purchase

--- contracts/Robots.sol
+++ contracts/Robots.sol
@@ -3,10 +3,33 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
-contract Robots is ERC721PresetMinterPauserAutoId {
+interface Garage {
+    function price() external returns (uint256);
+
+    function purchase() external payable;
+}
+
+contract Robots is ERC721PresetMinterPauserAutoId, Garage {
+    address public developer;
+    uint256 public constant override price = 1e18;
+
     constructor(
         string memory name,
         string memory symbol,
         string memory baseTokenURI
-    ) ERC721PresetMinterPauserAutoId(name, symbol, baseTokenURI) {}
+    ) ERC721PresetMinterPauserAutoId(name, symbol, baseTokenURI) {
+        _setupRole(MINTER_ROLE, address(this));
+        developer = msg.sender;
+    }
+
+    function purchase() public payable override {
+        require(msg.value >= price);
+
+        // Send ethers to developer
+        (bool success, ) = developer.call{value: msg.value}("");
+        require(success);
+
+        // Mint NFT (using external call)
+        this.mint(msg.sender);
+    }
 }
